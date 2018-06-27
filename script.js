6ef3a1c90d9725a761329af417af1abe3d5c06c5
imacoolgirlyo <tjrud1438@naver.com>
2018-06-27 13:58:01 +0900

    List delete 버튼 추가

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 
     window.onload = function () {
         var list = document.getElementsByClassName('list__info');
-        console.log(list); 
-       
+        console.log(list);
         for(var i =0; i<list.length ; i++){
            list[i].addEventListener('click', function(){
-               add_class(this)
+               add_class(this);
+               
            })
-            }
-
+            };
+            
         function add_class(e){
             console.log(e.classList);
             e.classList.toggle('list__name-checked');
             // e.classList.toggle('.list__name-checked');
         }
+        var close = this.document.getElementsByClassName('list__remove');
+        for(var i=0; i<close.length; i++){
+            close[i].addEventListener('click', function(){
+                remove_list(this);
+            })
+        }
+
+        function remove_list(e){
+            var div = e.parentElement;
+            div.style.display = "none";
+        }
+
+        
+        
        
         
     }
