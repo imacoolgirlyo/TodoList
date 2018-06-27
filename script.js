a175cf44a459564fbc034e139f8afe22238bb694
imacoolgirlyo <tjrud1438@naver.com>
2018-06-28 04:44:25 +0900

    list 추가 기능

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
     window.onload = function () {
         var list = document.getElementsByClassName('list__info');
-        console.log(list);
+        
         for(var i =0; i<list.length ; i++){
            list[i].addEventListener('click', function(){
                add_class(this);
@@ -10,10 +10,11 @@
             };
             
         function add_class(e){
-            console.log(e.classList);
+            
             e.classList.toggle('list__name-checked');
             // e.classList.toggle('.list__name-checked');
         }
+
         var close = this.document.getElementsByClassName('list__remove');
         for(var i=0; i<close.length; i++){
             close[i].addEventListener('click', function(){
@@ -26,8 +27,46 @@
             div.style.display = "none";
         }
 
-        
-        
+        var btn = document.getElementsByClassName('list__input-btn')[0];
+
+        btn.addEventListener('click', newElement);
+//      var value1 = document.getElementsByClassName('list__input-content')[0].value;
+
+        function newElement(){
+            var value1 = document.getElementsByClassName('list__input-content')[0].value;
+            var list__info = document.createElement("div");
+            var list__name = document.createElement("div");
+            var list__time = document.createElement("div");
+            var list__remove = document.createElement("div");
+
+
+            list__info.className = "list__info";
+            list__name.className = "list__name";
+            list__time.className = "list__time";
+            list__remove.className = "list__remove";
+
+            var container = document.getElementById('info-container').appendChild(list__info);
+
+            container.appendChild(list__name);
+            container.appendChild(list__time);
+            var remove_btn = container.appendChild(list__remove);
+
+            var remove_txt = document.createTextNode("x");
+            
+
+            remove_btn.appendChild(remove_txt);
+            var t = document.createTextNode(value1);
+            console.log(t);
+            list__name.appendChild(t);
+            
+                       
+
+            
+
+
+
+        }
+       
        
         
     }
