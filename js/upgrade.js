daa621ee5772f8e2e9549541e9b811c3eef333c3
imacoolgirlyo <tjrud1438@naver.com>
2018-07-24 15:48:40 +0900

    Make Code Beautiful

--- js/upgrade.js
+++ js/upgrade.js
@@ -1,125 +1,232 @@
 
-var input__form = document.getElementById('input__form');
+const popupContainer = document.querySelector('.popup__container'),
+    popupContainerdisplay = popupContainer.style.display,
+    showupBtn = document.querySelector('.plus__btn'),
+    form = document.querySelector(".js-form"),
+    textinput = document.querySelector('.list__input-content'),
+    importanceBtn = document.getElementsByName('importanceBtn');
+    unlist = document.querySelector('#todo'),
+    comlist = document.querySelector('#completed');
 
-document.getElementsByClassName("float")[0].addEventListener('click', function(){
-    input__form.style.display = 'block';
-})
+    console.log(importanceBtn);
 
+// Pop up 
+const changePopupDisplay = () => {
+    popupContainer.classList.toggle("noneToblock");
+}
 window.onclick = function(event){
-    if(event.target == input__form){
-        input__form.style.display = "none";
+    if(event.target == popupContainer){
+        popupContainer.classList.toggle("noneToblock");
     }
 }
+showupBtn.addEventListener('click', changePopupDisplay);
 
-// 새로운 아이템 추가
+const completeItem = event => {
+    
+    const item = event.target;
+    const li = item.parentNode;
+    const ul = li.parentNode;
 
-document.getElementById("save").addEventListener('click', function(){
-    var input = document.getElementById("input").value;
-    var toggle = document.getElementsByName('toggle');
+    if(ul === unlist){
+        comlist.appendChild(li);
+        li.classList.toggle('list__name-checked');
+    }else if (ul === comlist){
+        unlist.appendChild(li);
+        li.classList.toggle('list__name-checked');
+    }
+    
+}
+
+const removeItem = event => {
+    const item = event.target;
+    const li = item.parentNode;
+    const ul = li.parentNode;
+    ul.removeChild(li);
+}
+
+const addEvents = (listContent, removeBtn) => {
+    listContent.addEventListener('click', completeItem);
+    removeBtn.addEventListener('click', removeItem);
+}
 
 
-    if(input === ""){
-       var warning = document.getElementById("warningmsg");
-       warning.innerHTML = "You should write something ! "
+const createListitem = (todoText, importanceColor) => {
 
-    } else {
+    
+    const listItem = document.createElement('li');
+    listItem.classList.add('list__info');
 
-        for(var i =0 ; i< toggle.length ; i++){
-        if(toggle[i].checked == true){
-            additem(input, i)
+    const listContent = document.createElement('div');
+    listContent.classList.add('list__content');
+
+    const removeBtn = document.createElement('div');
+    removeBtn.classList.add('list__remove');
+    const x = document.createTextNode("X");
+    removeBtn.appendChild(x);
+
+    const textNode = document.createTextNode(todoText);
+
+    const importance = document.createElement('div');
+    importance.classList.add('list__importance');
+    importance.style.backgroundColor = importanceColor;
+
+
+    listItem.appendChild(importance);
+    listContent.appendChild(textNode);
+    listItem.appendChild(listContent);
+    listItem.appendChild(removeBtn);
+    
+    
+    unlist.appendChild(listItem);
+    addEvents(listContent,removeBtn);
+
+}
+
+const importanceCheck = () => {
+    var num = 0;
+    for(var i=0; i<importanceBtn.length; i++){
+        if(importanceBtn[i].checked == true){
+            num = i;
         }
     }
-        var warning = document.getElementById("warningmsg");
-        warning.innerHTML = ""
-        input__form.style.display = "none";
+    if(num == 0){
+        return "red";
+    }else if(num == 1){
+        return "yello";
+    }else if(num === 2){
+        return "blue";
     }
-
-    
-})
-
-function additem(v, r){
-
-    var importance = document.createElement("div");
-    importance.className = "list__importance";
-
-    if(r == "2"){
-        importance.classList.add("list__importance-green");
-    }else if(r == "1"){
-        importance.classList.add("list__importance-blue");
-    }
-
-    var item = document.createElement("li"); 
-    item.className = "list__info";
-    var content = document.createElement("div");
-    content.className = "list__content";
-
-    var txt = document.createTextNode(v);
-    content.appendChild(txt);
-
-    var icon = document.createElement("i");
-    icon.className = "fas fa-pen";
-    
-    var close = document.createElement("div"); 
-    close.className = "list__remove";
-    var closetxt = document.createTextNode("X");
-    close.appendChild(closetxt);
-
-    item.appendChild(importance);
-    item.appendChild(content);
-    item.appendChild(icon);
-    item.appendChild(close);
-    
-    document.getElementById("todo").appendChild(item);
-
-    document.getElementById("input").value = "";
-    
-    close.addEventListener('click', removeItem);
-    content.addEventListener('click', completeItem);
-    icon.addEventListener('click', EditItem);
-    
-    ChangeNum();
 
 }
 
-function ChangeNum(){
-    var todo = document.getElementById("todo");   
-    var num = todo.children.length;
-    var span = document.getElementById("number");
-    span.innerHTML = num + " Tasks";
-} 
+const handleFormSubmit = event => {
 
-
-function removeItem (){
-    
-    var ul = this.parentNode.parentNode; 
-    var li =this.parentNode; 
-    ul.removeChild(li);
-    ChangeNum();
+    const importanceColor = importanceCheck();
+    console.log(importanceColor);
+    const todoText = textinput.value;
+    createListitem(todoText, importanceColor);
+    textinput.value = "";
+    popupContainer.classList.toggle("noneToblock");
 }
 
-function completeItem (){
+// ( Add to do )버튼에 submit listener 추가
+form.addEventListener("submit", handleFormSubmit);
 
-    var ul = this.parentNode.parentNode; 
-    var li =this.parentNode; 
+
+
+// const savebtnClick = () => {
+//     const input = document.getElementById("input").value;
+//     const toggle = document.getElementsByName('toggle');
+
+//     if(input === ""){
+//        const warning = document.getElementById("warningmsg");
+//        warning.innerHTML = "You should write something ! "
+
+//     } else {
+
+//         for(var i =0 ; i< toggle.length ; i++){
+//         if(toggle[i].checked == true){
+//             additem(input, i)
+//         }
+//     }
+//         const warning = document.getElementById("warningmsg");
+//         warning.innerHTML = ""
+//         popupContainer.classList.toggle("noneToblock");
+//     }
+
+// };
+// document.getElementById("save").addEventListener('click', savebtnClick);
+
+
+
+// const additem = (v, r) => {
+
+//     const importance = document.createElement("div");
+//     importance.className = "list__importance";
+
+//     if(r == "2"){
+//         importance.classList.add("list__importance-green");
+//     }else if(r == "1"){
+//         importance.classList.add("list__importance-blue");
+//     }
+
+//     const item = document.createElement("li"); 
+//     item.className = "list__info";
+//     const content = document.createElement("div");
+//     content.className = "list__content";
+
+//     const txt = document.createTextNode(v);
+//     content.appendChild(txt);
+
+//     const icon = document.createElement("i");
+//     icon.className = "fas fa-pen";
     
-    var listID = ul.id;
+//     const close = document.createElement("div"); 
+//     close.className = "list__remove";
+//     const closetxt = document.createTextNode("X");
+//     close.appendChild(closetxt);
 
-    if(listID === "todo"){
-        ul.removeChild(li);
-        document.getElementById("completed").appendChild(li);
-        li.classList.add("list__name-checked");
+//     item.appendChild(importance);
+//     item.appendChild(content);
+//     item.appendChild(icon);
+//     item.appendChild(close);
+    
+//     document.getElementById("todo").appendChild(item);
+
+//     document.getElementById("input").value = "";
+    
+//     close.addEventListener('click', removeItem);
+//     content.addEventListener('click', completeItem);
+//     icon.addEventListener('click', EditItem);
+    
+//     ChangeNum();
+
+// }
+
+// const ChangeNum = () => {
+//     const todo = document.getElementById("todo");   
+//     const num = todo.children.length;
+//     const span = document.getElementById("number");
+//     span.innerHTML = num + " Tasks";
+// } 
+
+// const removeItem = (evt) => {
+//     const li = evt.target.parentNode;
+//     const ul = li.parentNode; 
+//     ul.removeChild(li);
+//     // ChangeNum();
+// }
+
+// const completeItem = (evt) => {
+//     const li = evt.target.parentNode;
+//     const ul = li.parentNode; 
+    
+//     var listID = ul.id;
+
+//     if(listID === "todo"){
+//         ul.removeChild(li);
+//         document.getElementById("completed").appendChild(li);
+//         li.classList.add("list__name-checked");
        
-    }
-    else{
-        ul.removeChild(li);
-        document.getElementById("todo").appendChild(li);
-        li.className = "list__info";
-    }
-    ChangeNum();
-}
+//     }
+//     else{
+//         ul.removeChild(li);
+//         document.getElementById("todo").appendChild(li);
+//         li.className = "list__info";
+//     }
+//     ChangeNum();
+// }
 
-function EditItem(){
-    console.log(this.parentNode);
-}
+// const EditItem = () => {
+//     console.log(this.parentNode);
+// }
 
-// 수정 말고, 입력할 떄 인풋 팝업, 색갈 선택 
+// // 새로운 아이템 추가
+
+
+
+
+
+
+
+// // 수정 말고, 입력할 떄 인풋 팝업, 색갈 선택 
